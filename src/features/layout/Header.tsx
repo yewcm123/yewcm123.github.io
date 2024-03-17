7994caf536115b04966a345f68a9797ba3e31555
Derrick Yew <yewcm123@gmail.com>
2024-03-17 11:54:50 +0800

    feat(header): added color change based on page on header

--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -1,17 +1,57 @@
+import { useEffect, useState } from "react";
+
 const Header = () => {
+    const height = window.innerHeight;
+        const [scrollY, setScrollY] = useState(0);
+        const [page, setPage] = useState(0)
+        const [headerCss, setHeaderCss] = useState("");
+
+        function handleScroll() {
+            setScrollY(window.scrollY);
+            console.log(scrollY)
+            if (scrollY > 1.88 * height) {
+                setPage(3);
+            } else if (scrollY > 0.88 * height) {
+                setPage(2);  
+            } else {
+                setPage(1);
+            }
+
+
+            if (page === 1) {
+                setHeaderCss("page-1")
+            } else if (page === 2) {
+                setHeaderCss("page-2")
+            } else if (page === 3) {
+                setHeaderCss("page-3")
+            }
+    
+        }
+
+        useEffect(() => {
+            window.addEventListener("scroll", handleScroll);
+            return () => {
+                window.removeEventListener("scroll", handleScroll);
+            };
+        })
+
     return (
-        <header>
+        <header className={headerCss}>
             <nav className="nav-bar">
-                <div className ="">
+                <div className="">
                     <a className="logo-title" href="/">
                         Derrick Yew
                     </a>
                 </div>
 
-                <div className="" >
+                <div className="">
                     <ul className="nav-item-list ">
                         <li className="">
-                            <a className="nav-item" aria-current="page" href="/">
+                            <a
+                                className="nav-item"
+                                aria-current="page"
+                                href="/"
+                            >
                                 about
                             </a>
                         </li>
